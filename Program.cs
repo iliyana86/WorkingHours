5410304ec45cf2df9b742f1a048fb84dd3942979
iliyana86 <133908422+iliyana86@users.noreply.github.com>
2024-02-12 13:39:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿int hour = int.Parse(Console.ReadLine());
+string week = Console.ReadLine();
+//hour = 0;
+if ((hour <= 18 && (week == "Monday") && hour >= 10))
+{
+    Console.WriteLine("open");
+}
+else if ((hour <= 18 && (week == "Tuesday") && hour >= 10))
+{
+    Console.WriteLine("open");
+}
+else if ((hour <= 18 && (week == "Wednesday") && hour >= 10))
+{
+    Console.WriteLine("open");
+}
+else if ((hour <= 18 && (week == "Thursday") && hour >= 10))
+{
+    Console.WriteLine("open");
+}
+else if ((hour <= 18 && (week == " Friday") && hour >= 10))
+{
+    Console.WriteLine("open");
+}
+else if ((hour <= 18 && (week == "Saturday") && hour >= 10))
+{
+    Console.WriteLine("open");
+}
+else if ((hour <= 18 && (week == "Sunday") && hour >= 10))
+{
+    Console.WriteLine("closed");
+}
+else
+{
+    Console.WriteLine("closed");
+}
+
+
+
+
+
+
+
+
+
+
+
